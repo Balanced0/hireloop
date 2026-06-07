1d88db268628b6c4838bd2087ff5fb9e781db2ef
balanced0 <alvichowdhury013@gmail.com>
2026-06-07 20:37:17 +0600

    feat: created PostJobPage component for job posting functionality

---
+++ src/app/dashboard/recruiter/jobs/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecruiterJobs = () => {
+    return (
+        <div>
+            Recruiter/ company all jobs
+        </div>
+    );
+};
+
+export default RecruiterJobs;
